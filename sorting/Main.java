eb703cb521b0fd9a698b212f16b1f010adfb7cce
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-04-26 17:04:56 +0200

    Added Heap Sort

--- sorting/Main.java
+++ sorting/Main.java
@@ -104,20 +104,82 @@ public class Main {
 
     public static void shellSort(int [] array,int n)
     {
+        //Get the length of the array
         int length = array.length;
+
+        //define the initial gap
         int gap = 3;
-        // Start with a big gap, then reduce the gap
+
+        // Start with a big gap, then reduce the gap by dividing it by 2
         for (int gap = n; gap > 0; gap /= 2)
         {
-            for (int i = gap; i < n; i++) {
+            // Iterate over the array starting from the current gap value
+            for (int i = gap; i < n; i++)
+            {
+                // Store the current element in a temporary variable
                 int temp = array[i];
                 int j;
+                // Move elements of array that are greater than temp into their respective positions
                 for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
                 {
                     array[j] = array[j - gap];
                 }
+                // Place temp (the current element being sorted) into its correct position
                 array[j] = temp;
             }
+        }
+    }
+
+    public static void heapSort(int[] array)
+    {
+        int length = array.length;
+
+        // Build heap (rearrange array)
+        for (int i = length / 2 - 1; i >= 0; i--)
+        {
+            heapify(arr, length, i)
+        }
+
+        // One by one extract an element from heap
+        for (int i = length - 1; i > 0; i--)
+        {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    private void heapify(int [] array, int length, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+        {
+            largest = l;
+        }
+
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+        {
+            largest = r;
+        }
+
+        // If largest is not root
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
         }
     }
 }
